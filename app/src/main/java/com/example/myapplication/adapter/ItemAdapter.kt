abe4f2495329c5bf02d1466cad9f3004c0953c62
杨建 <952801974@qq.com>
2021-10-10 17:58:23 +0800

    卡片布局 图片列表

--- app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
@@ -14,6 +15,7 @@ class ItemAdapter(private val context: Context, private val dataset:List<Affirma
 
     class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
         val textView:TextView = view.findViewById(R.id.item_title)
+        val imageView:ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -24,6 +26,7 @@ class ItemAdapter(private val context: Context, private val dataset:List<Affirma
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.resourceId)
+        holder.imageView.setImageResource(item.drawableId)
     }
 
     override fun getItemCount(): Int {
